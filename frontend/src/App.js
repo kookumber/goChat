d5d4fd875042d1b4dd0713e8b51ca92bcb390709
Quang Tran <quang.tran.769869@gmail.com>
2022-10-14 15:45:37 -0700

    Added React websocket connection to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { connect, sendMsg } from './api/index'
 
-function App() {
+const App = () => {
+
+  connect()
+
+  let send = () => {
+    console.log("Hello")
+    sendMsg("Hello")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={send}>Hit</button>
     </div>
-  );
+  )
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ frontend/src/api/index.js
@@ -0,0 +1,27 @@
+let socket = new WebSocket("ws://localhost:8080/ws")
+
+export const connect = () => {
+    console.log("Attempting Connection...")
+
+    socket.onopen = () => {
+        console.log("Successfully Connected")
+    }
+
+    socket.onmessage = (msg) => {
+        console.log(msg)
+    }
+
+    socket.onclose = event => {
+        console.log("Socket Closed Connection: ", event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket Error: ", error);
+    };
+}
+
+export const sendMsg = (msg) => {
+    console.log("Sending Message: ", msg)
+    socket.send(msg)
+}
+
